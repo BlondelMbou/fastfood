415e643c98c8a22a3eee04a4255df3d2458cfa4a
Folley-05 <aristotepascaldjounda@gmail.com>
2022-12-26 14:52:19 +0100

    change some text

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					Fast food app
 				</p>
 				<a
 					className="App-link"
