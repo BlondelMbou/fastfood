a0cc9170fc3ff9467adccdc7ce47f9f337d126a9
Folley-05 <aristotepascaldjounda@gmail.com>
2022-12-27 18:58:23 +0100

    add a button on registration and login forms

--- src/components/AppRoot.jsx
+++ src/components/AppRoot.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 import './animations.css'
 import Header from './Header/Header'
 import Hero from './Hero/Hero'
+import Login from './Login/Login'
 import Menu from './Menu/Menu'
+import Register from './Register/Register'
 
 function AppRoot() {
     return (
         <div className="approot">
             <Header />
-            <Hero />
-            <Menu />
+            {/* <Hero />
+            <Menu /> */}
+            <Login />
+            <Register />
         </div>
     )
 }
