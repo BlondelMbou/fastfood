b51ca483e3009851bdc4f0285f40751e7f0309d1
BlondelMbou <73158427+BlondelMbou@users.noreply.github.com>
2022-12-28 02:09:43 +0100

    import module

--- src/components/AppRoot.jsx
+++ src/components/AppRoot.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 
-import './animations.css'
-import Header from './Header/Header'
-import Hero from './Hero/Hero'
-import Login from './Login/Login'
-import Menu from './Menu/Menu'
-import Register from './Register/Register'
+import './animations.css';
+import Header from './Header/Header';
+import Hero from './Hero/Hero';
+import Login from './Login/Login';
+import Menu from './Menu/Menu';
+import Register from './Register/Register';
+import AboutUs from './AboutUs/AboutUs';
+import Testimony from './Testimony/Testimony';
+import Reservation from './Reservation/Reservation';
+import Footer from './Footer/Footer';
 
 function AppRoot() {
-    return (
-        <div className="approot">
-            <Header />
-            <Hero />
-            <Menu />
-            <AboutUs />
-            <Testimony />
-            <Reservation />
-            <Login />
-            <Register />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="approot">
+      <Header />
+      <Hero />
+      <Menu />
+      <AboutUs />
+      <Testimony />
+      <Reservation />
+      <Login />
+      <Register />
+      <Footer />
+    </div>
+  );
 }
+
+export default AppRoot;
