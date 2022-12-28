c2cebdec1dfe972b5d7bf648564d329be12e2660
BlondelMbou <73158427+BlondelMbou@users.noreply.github.com>
2022-12-28 03:38:37 +0100

    modification

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,45 @@
 /* The header component of application */
 
-import React from 'react'
+import React from 'react';
 
-import './header.css'
+import './header.css';
 
 function Header() {
+  const toogleMenu = () => {
+    document.querySelector('.toggle_menu').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('responsive');
+  };
+  return (
+    <header>
+      <div class="logo">
+        <p>
+          <span>Your</span>Dish
+        </p>
+      </div>
+      <ul class="menu">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+        <li>
+          <a href="#about_us">About Us</a>
+        </li>
+        <li>
+          <a href="#reservation">Reservation</a>
+        </li>
+        <li>
+          <a href="#home">Login</a>
+        </li>
+        <li>
+          <a href="#menu">Registration</a>
+        </li>
+      </ul>
 
-    const toogleMenu=()=>{
-        document.querySelector('.toggle_menu').classList.toggle('active')
-        document.querySelector('.menu').classList.toggle('responsive')
-    }
-    return (
-        <header>
-            <div class="logo">
-                <p><span>Your</span>Dish</p>
-            </div>
-            <ul class="menu">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#menu">Menu</a></li>
-                <li><a href="#about_us">About Us</a></li>
-                <li><a href="#reservation">Reservation</a></li>
-                <li><a href="#home">Login</a></li>
-                <li><a href="#menu">Registration</a></li>
-            </ul>
-
-            <div class="toggle_menu" onClick={toogleMenu} />
-        </header>
-    )
+      <div class="toggle_menu" onClick={toogleMenu} />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
